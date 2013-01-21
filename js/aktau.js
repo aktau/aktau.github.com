3bfcfbdf22fcd7163045bdfe85937b664295619e
Aktau <nicolashillegeer@gmail.com>
2013-01-21 12:29:17 +0100

    Changed header line, added cube rendering, fixed box model again

--- js/aktau.js
+++ js/aktau.js
@@ -1,10 +1,61 @@
-var backgroundElem;
+/* Colors */
 
-function linePath(x1, y1, x2, y2) { 
-	return 'M' + x1 + ' ' + y1 + ' L' + x2 + ' ' + y2; 
+var solarized = {
+	"background": "#002b36",
+	"background2": "#073642"
+};
+
+var palette = solarized;
+
+function brighten(hex, percent){
+    // strip the leading # if it's there
+    hex = hex.replace(/^\s*#|\s*$/g, '');
+
+    // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+    if(hex.length == 3){
+        hex = hex.replace(/(.)/g, '$1$1');
+    }
+
+    var r = parseInt(hex.substr(0, 2), 16),
+        g = parseInt(hex.substr(2, 2), 16),
+        b = parseInt(hex.substr(4, 2), 16);
+
+    return '#' +
+       ((0|(1<<8) + r + (256 - r) * percent / 100).toString(16)).substr(1) +
+       ((0|(1<<8) + g + (256 - g) * percent / 100).toString(16)).substr(1) +
+       ((0|(1<<8) + b + (256 - b) * percent / 100).toString(16)).substr(1);
 }
 
-function smoothPath(obj1, obj2) {	
+function darken(hex, percent){
+    // strip the leading # if it's there
+    hex = hex.replace(/^\s*#|\s*$/g, '');
+
+    // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+    if(hex.length == 3){
+        hex = hex.replace(/(.)/g, '$1$1');
+    }
+
+    var r = parseInt(hex.substr(0, 2), 16),
+        g = parseInt(hex.substr(2, 2), 16),
+        b = parseInt(hex.substr(4, 2), 16);
+
+    return '#' +
+       ((0|(1<<8) + r * (100 - percent) / 100).toString(16)).substr(1) +
+       ((0|(1<<8) + g + (100 - percent) / 100).toString(16)).substr(1) +
+       ((0|(1<<8) + b + (100 - percent) / 100).toString(16)).substr(1);
+}
+
+/* Raphael */
+
+// https://developer.mozilla.org/en-US/docs/SVG/Tutorial/Paths
+
+var backgroundElem;
+
+function linePath(x1, y1, x2, y2) {
+	return 'M' + x1 + ' ' + y1 + ' L' + x2 + ' ' + y2;
+}
+
+function smoothPath(obj1, obj2) {
 	var bb1 = obj1.getBBox(),
 		bb2 = obj2.getBBox(),
 		p = [{x: bb1.x + bb1.width / 2, y: bb1.y - 1},
@@ -15,16 +66,16 @@ function smoothPath(obj1, obj2) {
 		{x: bb2.x + bb2.width / 2, y: bb2.y + bb2.height + 1},
 		{x: bb2.x - 1, y: bb2.y + bb2.height / 2},
 		{x: bb2.x + bb2.width + 1, y: bb2.y + bb2.height / 2}],
-		d = {}, 
+		d = {},
 		dis = [],
 		dx,
 		dy;
-		
+
 	for (var i = 0; i < 4; ++i) {
 		for (var j = 4; j < 8; ++j) {
 			dx = Math.abs(p[i].x - p[j].x);
 			dy = Math.abs(p[i].y - p[j].y);
-			
+
 			if ((i == j - 4) || (((i != 3 && j != 6) || p[i].x < p[j].x) && ((i != 2 && j != 7) || p[i].x > p[j].x) && ((i !== 0 && j != 5) || p[i].y > p[j].y) && ((i != 1 && j != 4) || p[i].y < p[j].y))) {
 				dis.push(dx + dy);
 				d[dis[dis.length - 1]] = [i, j];
@@ -33,26 +84,26 @@ function smoothPath(obj1, obj2) {
 	}
 
 	var res = (dis.length === 0) ? [0, 4] : d[Math.min.apply(Math, dis)];
-	
+
 	var x1 = p[res[0]].x,
 		y1 = p[res[0]].y,
 		x4 = p[res[1]].x,
 		y4 = p[res[1]].y;
-	
+
 	dx = Math.max(Math.abs(x1 - x4) / 2, 10);
 	dy = Math.max(Math.abs(y1 - y4) / 2, 10);
-	
+
 	// note: toFixed(3) means that 3 numbers will be left after the decimal sign
 	var x2 = [x1, x1, x1 - dx, x1 + dx][res[0]].toFixed(3),
 		y2 = [y1 - dy, y1 + dy, y1, y1][res[0]].toFixed(3),
 		x3 = [0, 0, 0, 0, x4, x4, x4 - dx, x4 + dx][res[1]].toFixed(3),
 		y3 = [0, 0, 0, 0, y1 + dy, y1 - dy, y4, y4][res[1]].toFixed(3);
-		
+
 	// draw a smooth path using curves, control points having been calculated before
 	// the hiddenpath is a path that goes from/to the middles of the objects in question, so that an animateAlong can look nice
 	var shortpath = ["M", x1.toFixed(3), y1.toFixed(3), "C", x2, y2, x3, y3, x4.toFixed(3), y4.toFixed(3)].join(","),
 		longpath = ["M", p[0].x.toFixed(3), p[2].y.toFixed(3), "C", x2, y2, x3, y3, p[4].x.toFixed(3), p[6].y.toFixed(3)].join(",");
-	
+
 	return {'shortpath': shortpath, 'longpath': longpath};
 }
 
@@ -70,7 +121,7 @@ function hexagon(radius) {
 			radians = degrees * deg2rad,
 			x = radius * Math.cos(radians),
 			y = radius * Math.sin(radians);
-		
+
 		path += (i === 0 ? "M" : "L") + x + "," + y;
 	}
 
@@ -81,11 +132,12 @@ function hexagon(radius) {
 
 // returns 3 SVG paths representing the faces of the cube
 // r.cube(50).forEach(function(e) { e.attr({"transform": "T100,100","fill": "red"}) })
-function cube(length) {
+function cube(length, startAngle) {
 	var faces = [],
 		faceIndex = 0,
-		startAngle = 90,
 		deg2rad = Math.PI / 180;
+
+	startAngle = startAngle || 90;
 
 	for (var i = 0; i < 3; ++i) {
 		var face = "";
@@ -122,20 +174,23 @@ Raphael.fn.cube = function(length) {
 		faces.push(this.path(facePaths[i]));
 	}
 
-	return faces;
+	return {
+		"faces": faces,
+		"cube": this.set().push(faces[0], faces[1], faces[2])
+	};
 };
 
 Raphael.fn.randomPoint = function() {
 	return {
 		"x": rangeRandom(0, backgroundElem.width()),
-		"y": rangeRandom(0, backgroundElem.height()) 
+		"y": rangeRandom(0, backgroundElem.height())
 	};
 };
 
 Raphael.fn.randomCircle = function(x, y, r) {
 	return this.circle(x, y, 1)
 		.attr({"stroke-opacity": 1, stroke: "#FFF"})
-		.animate({r: r, "stroke-width": 20}, 5000);	
+		.animate({r: r, "stroke-width": 20}, 5000);
 };
 
 Raphael.fn.manyCircles = function(n) {
@@ -150,8 +205,8 @@ Raphael.fn.manyCircles = function(n) {
 	return circles;
 };
 
-Raphael.fn.line = function(x1, y1, x2, y2, attr) { 
-	return this.path(linePath(x1, y1, x2, y2)).attr(attr || {stroke: 'white'}); 
+Raphael.fn.line = function(x1, y1, x2, y2, attr) {
+	return this.path(linePath(x1, y1, x2, y2)).attr(attr || {stroke: 'white'});
 };
 
 Raphael.fn.connection = function (obj1, obj2, attr) {
@@ -166,7 +221,7 @@ Raphael.fn.connection = function (obj1, obj2, attr) {
  * var starSpin = function () {
  *     logoStar.attr({rotation: 0}).animate({rotation: 360}, 5000, starSpin);
  * }
- * 
+ *
  * Raphael v. 2
  * var spin = Raphael.animation({transform: "r360"}, 2500).repeat(Infinity);
  * myElement.animate(spin);
@@ -195,9 +250,9 @@ window.onload = function () {
 
 		circles.push(r.randomCircle(rp.x, rp.y, 50));
 
-		r.hexagon(60)
-			.attr({"stroke-width": 10, "stroke": "white", "fill": "white"})
-			.animate({"transform": "t" + rp.x + "," + rp.y}, 3000);
+		// r.hexagon(60)
+		// 	.attr({"stroke-width": 10, "stroke": "white", "fill": "white"})
+		// 	.animate({"transform": "t" + rp.x + "," + rp.y}, 3000);
 	}
 
 	for (i = 0, len = circles.length; i < len; ++i) {
@@ -208,25 +263,30 @@ window.onload = function () {
 
 		console.log("animating from " + circle.getBBox().x + " to "  + (circle.getBBox().x + 40));
 
-		circle.animate({"cx": cx + 80, "cy": cy + 80}, 1000);				
+		circle.animate({"cx": cx + 80, "cy": cy + 80}, 1000);
 	}
 
 	var radius = 50,
-		startx = 100,
-		starty = 100,
+		startx = 500,
+		starty = 200,
 		xstep = radius * Math.sqrt(3),
 		ystep = radius * (1.5), // radius
-		steps = 7,
+		steps = 6,
 		alternate = 0,
-		cubes = []; 
+		cubes = [];
 
 	for (i = 0; i <= steps; ++i) {
 		var dy = starty + i * ystep;
 
 		for (j = 0; j <= steps; ++j) {
 			var dx = startx + j * xstep,
-				c = r.cube(50)
-				.forEach(function(e) { e.attr({"transform": "T" + dx + "," + dy, "fill": "red"}) });
+				c = r.cube(50);
+
+			console.log("going for it...");
+			console.log(c);
+			c.cube.attr({"transform": "T" + dx + "," + dy, "fill": ((i + j) % 2 === 0) ? "red" : "orange"});
+			c.faces[0].attr({"fill": darken(palette.background2, 25)});
+			c.faces[1].attr({"fill": brighten(palette.background2, 25)});
 
 			cubes.push(c);
 		}
@@ -234,5 +294,26 @@ window.onload = function () {
 		startx += ((++alternate % 2 === 0) ? 1 : -1) * (xstep / 2);
 	}
 
-	// animation = window.setInterval(animate, 10);
+	/*
+	var cubeAngle = 0;
+
+	function animate() {
+		// console.log("animating...");
+		//console.log(arguments);
+		cubes.forEach(function(c) {
+			var newCube = cube(50, 0 + cubeAngle);
+
+			cubeAngle = (cubeAngle + 45) % 360;
+
+			for (var i = 0; i < c.faces.length; i++) {
+				c.faces[i].animate({"path": newCube[i]}, 2000);
+			}
+
+			//c.cube.animate({"fill": Raphael.getColor()}, 400);
+		});
+	}
+
+	var animation = window.setInterval(animate, 2000);
+
+	*/
 }
