4a658a007896dfa2601ea31759555a1112389b88
Aktau <nicolashillegeer@gmail.com>
2013-02-06 22:48:52 +0100

    Created vagrant setup

---
+++ vagrant/manifests/default.pp
@@ -0,0 +1,25 @@
+# Basic Puppet Apache manifest
+
+class apache {
+  exec { 'apt-get update':
+    command => '/usr/bin/apt-get update'
+  }
+
+  package { "apache2":
+    ensure => present,
+  }
+
+  service { "apache2":
+    ensure => running,
+    require => Package["apache2"],
+  }
+
+  file { '/var/www':
+    ensure => link,
+    target => "/vagrant_docroot",
+    notify => Service['apache2'],
+    force  => true
+  }
+}
+
+include apache
