d7106cb851102d40b329cebb5d99a7a2c5701de6
Aktau <nicolashillegeer@gmail.com>
2013-08-07 21:23:11 +0200

    add tags to the front page

--- lib/default.rb
+++ lib/default.rb
@@ -8,15 +8,40 @@ module PostHelper
 
   def get_post_start(post)
     content = post.compiled_content
+
     if content =~ /\s<!-- more -->\s/
       content = content.partition('<!-- more -->').first +
         "<div class='read-more'>" + link_to('Continue reading &rsaquo;', post.path) + "</div>"
     end
+
     return content
   end
 end
 
+module PageHelper
+  def get_all_tags(items)
+    tags = Hash.new
+
+    items.each do |item|
+        if item[:tags]
+            item[:tags].each { |t|
+              name = t.downcase
+
+              if tags.has_key?(name)
+                tags[name] = { :count => tags[name][:count] + 1}
+              else
+                tags[name] = { :count => 1 }
+              end
+            }
+        end
+    end
+
+    return tags
+  end
+end
+
 include PostHelper
+include PageHelper
 
 include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::Tagging
