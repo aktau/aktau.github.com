be74e0b96d668f732c2f51d2adc6a66bb6de3d6a
Aktau <nicolashillegeer@gmail.com>
2013-08-07 14:31:18 +0200

    functions for pretty dates and cutoff posts

--- lib/default.rb
+++ lib/default.rb
@@ -1,6 +1,23 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 
+module PostHelper
+  def get_pretty_date(post)
+    attribute_to_time(post[:created_at]).strftime('%B %-d, %Y')
+  end
+
+  def get_post_start(post)
+    content = post.compiled_content
+    if content =~ /\s<!-- more -->\s/
+      content = content.partition('<!-- more -->').first +
+        "<div class='read-more'>" + link_to('Continue reading &rsaquo;', post.path) + "</div>"
+    end
+    return content
+  end
+end
+
+include PostHelper
+
 include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::Tagging
 include Nanoc3::Helpers::Rendering
