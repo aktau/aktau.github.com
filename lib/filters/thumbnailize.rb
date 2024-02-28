1ee475293444030da28f08b42851fa24a6069eda
Nicolas Hillegeer <nicolas@hillegeer.com>
2024-02-28 13:13:16 +0100

    nanoc: support Linux platform in thumbnailize
    Source: https://nanoc.app/doc/guides/using-binary-items-effectively.

--- lib/filters/thumbnailize.rb
+++ lib/filters/thumbnailize.rb
@@ -3,13 +3,25 @@ class Thumbnailize < Nanoc::Filter
   type       :binary
 
   def run(filename, params={})
-    system(
-      'sips',
-      '-Z',
-      params[:width].to_s,
-      '--out',
-      output_filename,
-      filename
-    )
+    if RUBY_PLATFORM =~ /linux/
+        system(
+          'convert',
+          '-resize',
+          params[:width].to_s,
+          filename,
+          output_filename
+        )
+    elsif RUBY_PLATFORM =~ /darwin/
+        system(
+          'sips',
+          '-Z',
+          params[:width].to_s,
+          '--out',
+          output_filename,
+          filename
+        )
+    else
+        raise RuntimeError.new("BUG: platform not supported yet, please add")
+    end
   end
 end
